71296fd442dad5f091b1839cf11dd6a16549081c
CHUNG YUNSEONG <e0550449@u.nus.edu>
2023-07-04 09:05:03 +0800

    Time: 5 ms (42.53%), Space: 44.4 MB (11.4%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i : nums) {
+            map.merge(i, 1, (a, b) -> a + 1);
+        }
+        for (int i : map.keySet()) {
+            if (map.get(i) == 1) {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
