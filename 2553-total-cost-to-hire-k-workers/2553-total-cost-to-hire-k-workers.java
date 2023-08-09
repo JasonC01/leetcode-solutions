71ed8a7bdf115171f889381d19dcdbc9869ee375
CHUNG YUNSEONG <e0550449@u.nus.edu>
2023-08-09 15:29:48 +0800

    Time: 54 ms (95.23%), Space: 57.6 MB (57.75%) - LeetHub

--- 2553-total-cost-to-hire-k-workers/2553-total-cost-to-hire-k-workers.java
+++ 2553-total-cost-to-hire-k-workers/2553-total-cost-to-hire-k-workers.java
@@ -1,56 +1,45 @@
 class Solution {
     public long totalCost(int[] costs, int k, int candidates) {
-        int total = costs.length;
-        if (total <= 2 * candidates) {
-            Arrays.sort(costs);
-            int ans = 0;
-            for (int i = 0; i < k; i++) {
-                ans += costs[i];
-            }
-            return ans;
+        Deque<Integer> q = new ArrayDeque<>();
+        for (int i : costs) {
+            q.add(i);
         }
-        long ans = 0;
-        int leftIndex = 0;
-        int rightIndex = total - 1;
-        PriorityQueue<Integer> left = new PriorityQueue<>();
-        PriorityQueue<Integer> right = new PriorityQueue<>();
-        for (int i = leftIndex; i < candidates; i++) {
-            left.add(costs[i]);
+        PriorityQueue<Integer> first = new PriorityQueue<>();
+        PriorityQueue<Integer> second = new PriorityQueue<>();
+        while (first.size() < candidates && !q.isEmpty()) {
+            first.add(q.pollFirst());
         }
-        for (int i = rightIndex; i > Math.max(leftIndex, rightIndex - candidates); i--) {
-            right.add(costs[i]);
+        while (second.size() < candidates && !q.isEmpty()) {
+            second.add(q.pollLast());
         }
-        leftIndex = candidates - 1;
-        rightIndex = Math.max(leftIndex, rightIndex - candidates) + 1;
-        for (int i = 0; i < k; i++) {
-            if (left.isEmpty()) {
-                ans += right.poll();
-                rightIndex--;
-                if (rightIndex > leftIndex) {
-                    right.add(costs[rightIndex]);
-                }
-            } else if (right.isEmpty()) {
-                ans += left.poll();
-                leftIndex++;
-                if (leftIndex < rightIndex) {
-                    left.add(costs[leftIndex]);
-                }
+        long cost = 0;
+        int hired = 0;
+        boolean hiredFirst = true;
+        while (hired < k) {
+            if (second.isEmpty()) {
+                cost += first.poll();
+                hiredFirst = true;
+            } else if (first.isEmpty()) {
+                cost += second.poll();
+                hiredFirst = false;
             } else {
-                if (left.peek() <= right.peek()) {
-                    ans += left.poll();
-                    leftIndex++;
-                    if (leftIndex < rightIndex) {
-                        left.add(costs[leftIndex]);
-                    }
+                if (first.peek() <= second.peek()) {
+                    cost += first.poll();
+                    hiredFirst = true;
                 } else {
-                    ans += right.poll();
-                    rightIndex--;
-                    if (rightIndex > leftIndex) {
-                        right.add(costs[rightIndex]);
-                    }
+                    cost += second.poll();
+                    hiredFirst = false;
                 }
             }
+            if (!q.isEmpty()) {
+                if (hiredFirst) {
+                    first.add(q.pollFirst());
+                } else {
+                    second.add(q.pollLast());
+                }
+            }
+            hired++;
         }
-        return ans;
+        return cost;
     }
 }
