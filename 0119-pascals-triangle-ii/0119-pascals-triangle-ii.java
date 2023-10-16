2840ebfba1134ded2eca56cc7191e2bd389e7b35
CHUNG YUNSEONG <e0550449@u.nus.edu>
2023-10-16 10:58:53 +0800

    Time: 1 ms (79.7%), Space: 39.7 MB (97.27%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
@@ -3,15 +3,13 @@ class Solution {
         List<Integer> prevRow = new ArrayList<>();
         prevRow.add(1);
         for (int i = 0; i < rowIndex; i++) {
-            System.out.println(prevRow);
             List<Integer> currRow = new ArrayList<>();
-            currRow.add(prevRow.get(0));
+            currRow.add(1);
             for (int j = 1; j < prevRow.size(); j++) {
                 currRow.add(prevRow.get(j - 1) + prevRow.get(j));
             }
-            currRow.add(prevRow.get(prevRow.size() - 1));
-            prevRow.clear();
-            prevRow.addAll(currRow);
+            currRow.add(1);
+            prevRow = currRow;
         }
         return prevRow;
     }
