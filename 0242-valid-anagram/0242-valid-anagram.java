22eac5332a7a7ca13d73a3c908b3d59a12ba9880
CHUNG YUNSEONG <e0550449@u.nus.edu>
2023-12-16 09:56:11 +0800

    Time: 5 ms (54.58%), Space: 42.3 MB (74.32%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] counter = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            counter[s.charAt(i) - 97]++;
+            counter[t.charAt(i) - 97]--;
+        }
+        for (int i : counter) {
+            if (i != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
