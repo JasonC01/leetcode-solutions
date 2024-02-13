85fdb78ec2c1310a44b5983f48b57893c04f4f9b
CHUNG YUNSEONG <e0550449@u.nus.edu>
2024-02-13 23:25:27 +0800

    Time: 2 ms (71.53%), Space: 44.8 MB (76.00%) - LeetHub

---
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String firstPalindrome(String[] words) {
+        for (String s : words) {
+            if (isPalindrome(s)) {
+                return s;
+            }
+        }
+        return "";
+    }
+    
+    public boolean isPalindrome(String s) {
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
