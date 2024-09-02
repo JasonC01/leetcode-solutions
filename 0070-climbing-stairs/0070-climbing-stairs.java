287871f447b267331683cbd26edc88cde48e12e9
CHUNG YUNSEONG <e0550449@u.nus.edu>
2024-09-02 21:37:15 +0800

    Time: 0 ms (100%), Space: 40.2 MB (68.35%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,11 +1,11 @@
 class Solution {
     int[] memo = new int[46];
     public int climbStairs(int n) {
-        if (n == 0) {
-            return 1;
-        }
         if (n < 0) {
             return 0;
+        }
+        if (n <= 1) {
+            return 1;
         }
         if (memo[n] != 0) {
             return memo[n];
