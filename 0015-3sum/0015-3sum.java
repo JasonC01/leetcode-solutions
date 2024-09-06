6041180864e3e5cf5ca83985a10c9a10ef3a2609
CHUNG YUNSEONG <e0550449@u.nus.edu>
2024-09-06 16:53:04 +0800

    Time: 668 ms (21.1%), Space: 53 MB (12.16%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        HashSet<List<Integer>> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            List<Integer> res = new ArrayList<>();
+            int low = i + 1;
+            int high = nums.length - 1;
+            int target = -nums[i];
+            
+            while (low < high) {
+                int sum = nums[low] + nums[high];
+                if (sum == target) {
+                    res.add(nums[i]);
+                    res.add(nums[low]);
+                    res.add(nums[high]);
+                    set.add(res);
+                    res = new ArrayList<>();
+                     low++;
+                } else if (sum < target) {
+                    low++;
+                } else {
+                    high--;
+                }
+            }
+        }
+        return new ArrayList<>(set);
+    }
+}
