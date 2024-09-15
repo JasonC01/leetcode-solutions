f15aa0b061909d8c0cfcef002ca84f0616c4c452
CHUNG YUNSEONG <e0550449@u.nus.edu>
2024-09-16 00:04:10 +0800

    Time: 43 ms (56.94%), Space: 47.5 MB (46.26%) - LeetHub

---
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int findTheLongestSubstring(String s) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int currentMask = 0;
+        int currentIndex = 0;
+        int ans = 0;
+        map.put(0, -1);
+        for (char c : s.toCharArray()) {
+            if (isVowel(c)) {
+                int mask = currentMask ^ getMask(c);
+                if (map.containsKey(mask)) {
+                    ans = Math.max(ans, currentIndex - map.get(mask));
+                } else {
+                    map.put(mask, currentIndex);
+                }
+                currentMask = mask;
+            } else {
+                if (map.containsKey(currentMask)) {
+                    ans = Math.max(ans, currentIndex - map.get(currentMask));
+                } else {
+                    map.put(currentMask, currentIndex);
+                }
+            }
+            currentIndex++;
+        }
+        return ans;
+    }
+
+    public boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+
+    public int getMask(char c) {
+        return c == 'a' ? 16 : c == 'e' ? 8 : c == 'i' ? 4 : c == 'o' ? 2 : 1;
+    }
+}
