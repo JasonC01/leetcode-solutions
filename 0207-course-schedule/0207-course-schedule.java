48104e86c98028f01710a51aba06a5b9016e82f2
CHUNG YUNSEONG <e0550449@u.nus.edu>
2024-09-16 18:50:09 +0800

    Time: 10 ms (15.49%), Space: 45.4 MB (36.93%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,42 +1,36 @@
 class Solution {
-    List<List<Integer>> prereq = new ArrayList<>();
-    List<List<Integer>> adjList = new ArrayList<>();
-    boolean[] completed;
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        completed = new boolean[numCourses];
-        for (int i = 0; i < numCourses; i++) {
-            prereq.add(new ArrayList<>());
-            adjList.add(new ArrayList<>());
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        int[] inCount = new int[numCourses];
+        boolean[] visited = new boolean[numCourses];
+        for (int[] prereq : prerequisites) {
+            int course = prereq[0];
+            int req = prereq[1];
+            if (!map.containsKey(req)) {
+                map.put(req, new ArrayList<>());
+            }
+            map.get(req).add(course);
+            inCount[course]++;
         }
-        for (int[] prerequisite : prerequisites) {
-            int from = prerequisite[0];
-            int to = prerequisite[1];
-            prereq.get(to).add(from);
-            adjList.get(from).add(to);
-        }
-        List<Integer> canComplete = new ArrayList<>();
-        for (int i = 0; i < numCourses; i++) {
-            if (prereq.get(i).isEmpty()) {
-                canComplete.add(i);
+        Queue<Integer> q = new ArrayDeque<>();
+        for (int i = 0; i < inCount.length; i++) {
+            if (inCount[i] == 0) {
+                q.add(i);
             }
         }
-        for (int i : canComplete) {
-            completeCourse(i);
-        }
-        boolean ans = true;
-        for (boolean b : completed) {
-            ans &= b;
-        }
-        return ans;
-    }
-
-    public void completeCourse(int currCourse) {
-        if (prereq.get(currCourse).isEmpty()) {
-            completed[currCourse] = true;
-            for (int adj : adjList.get(currCourse)) {
-                prereq.get(adj).remove((Integer) currCourse);
-                completeCourse(adj);
+        int ans = 0;
+        while (!q.isEmpty()) {
+            int course = q.poll();
+            if (inCount[course] != 0 || visited[course]) {
+                continue;
+            }
+            visited[course] = true;
+            ans++;
+            for (int i : map.getOrDefault(course, new ArrayList<>())) {
+                inCount[i]--;
+                q.add(i);
             }
         }
+        return ans == numCourses;
     }
 }
