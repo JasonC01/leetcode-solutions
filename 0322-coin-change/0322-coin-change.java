9467886ec721586d1a93911adff7afc724485b68
CHUNG YUNSEONG <e0550449@u.nus.edu>
2024-09-28 21:57:33 +0800

    Time: 14 ms (88.6%), Space: 43.4 MB (99.68%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,29 +1,27 @@
 class Solution {
-    int[][] memo;
     public int coinChange(int[] coins, int amount) {
-        memo = new int[coins.length][amount + 1];
-        for (int[] i : memo) {
-            Arrays.fill(i, -1);
+        int[] memo = new int[amount + 1];
+        Arrays.fill(memo, Integer.MAX_VALUE);
+        memo[0] = 0;
+        for (int coin : coins) {
+            if (coin <= amount) {
+                memo[coin] = 1;
+            }
         }
-        int res = helper(coins, 0, amount, 0);
-        return res == Integer.MAX_VALUE ? -1 : res;
-    }
-
-    public int helper(int[] coins, int index, int amount, int count) {
-        if (index > coins.length - 1) {
-            return amount == 0 ? count : Integer.MAX_VALUE;
+        for (int i = 1; i <= amount; i++) {
+            int val = Integer.MAX_VALUE;
+            for (int coin : coins) {
+                if (i < coin) {
+                    continue;
+                }
+                int counterpart = memo[i - coin];
+                if (counterpart == Integer.MAX_VALUE) {
+                    continue;
+                }
+                val = Math.min(val, counterpart + 1);
+            }
+            memo[i] = val;
         }
-        if (memo[index][amount] != -1) {
-            return memo[index][amount] == Integer.MAX_VALUE ? Integer.MAX_VALUE : memo[index][amount] + count;
-        }
-        int ans = Integer.MAX_VALUE;
-        int coinCount = 0;
-        int minCount = Integer.MAX_VALUE;
-        while (coinCount * coins[index] <= amount) {
-            ans = Math.min(ans, helper(coins, index + 1, amount - (coinCount * coins[index]), coinCount + count));
-            coinCount++;
-        }
-        memo[index][amount] = ans == Integer.MAX_VALUE ? Integer.MAX_VALUE : ans - count;
-        return ans;
+        return memo[amount] == Integer.MAX_VALUE ? -1 : memo[amount];
     }
 }
