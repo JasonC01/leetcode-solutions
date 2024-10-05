97786e3b9b6ee8cdc5b6ac844909abef794c5e7e
CHUNG YUNSEONG <e0550449@u.nus.edu>
2024-10-06 00:05:36 +0800

    Time: 42 ms (25.65%), Space: 45 MB (20.4%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,39 +1,20 @@
 class Solution {
      public boolean checkInclusion(String s1, String s2) {
-        if (s2.length() < s1.length()) {
-            return false;
+        int[] counter = new int[26];
+        char[] cArr1 = s1.toCharArray();
+        char[] cArr2 = s2.toCharArray();
+        for (char c : cArr1) {
+            counter[c - 97] += 1;
         }
-        HashMap<Character, Integer> counter = new HashMap<>();
-        for (char c : s1.toCharArray()) {
-            counter.merge(c, 1, (a, b) -> a + 1);
-        }
-        int low = -1;
-        int high = 0;
-        while (low < high && high < s2.length()) {
-            // System.out.println(high);
-            // System.out.println(low);
-            // System.out.println("//");
-            char curr = s2.charAt(high);
-            if (high - low - 1 == s1.length()) {
+        for (int i = 0; i <= s2.length() - s1.length(); i++) {
+            int[] counter2 = new int[26];
+            for (int j = i; j < i + s1.length(); j++) {
+                counter2[cArr2[j] - 97] += 1;
+            }
+            if (Arrays.equals(counter, counter2)) {
                 return true;
             }
-            if (!counter.containsKey(curr)) {
-                for (++low; low < high; low++) {
-                    counter.merge(s2.charAt(low), 1, (a, b) -> a + 1);
-                }
-                high++;
-            } else {
-                if (counter.get(curr) > 0) {
-                    counter.merge(curr, 0, (a, b) -> a - 1);
-                    
-                } else {
-                    while (++low < high && s2.charAt(low) != curr) {
-                        low++;
-                    }
-                }
-                high++;
-            }
         }
-        return high - low - 1 == s1.length();
+        return false;
     }
 }
