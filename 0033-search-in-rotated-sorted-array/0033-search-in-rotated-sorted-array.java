6443f2f76425c23326602f707978d353441409b7
CHUNG YUNSEONG <e0550449@u.nus.edu>
2024-10-09 13:44:41 +0800

    Time: 5 ms (100%), Space: 42.4 MB (13.05%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,34 +1,38 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int low = findStart(nums);
-        int high = low + nums.length - 1;
-        int n = nums.length;
-        while (low < high) {
-            int mid = ((low + high) / 2);
-            if (nums[mid % n] == target) {
-                return mid % n;
-            } else if (nums[mid % n] < target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
-        }
-        return nums[low % n] == target ? low % n : -1;
-    }
-    public int findStart(int[] nums) {
         int low = 0;
         int high = nums.length - 1;
         while (low < high) {
-            int mid = (low + high) / 2;
-            if (nums[mid] >= nums[low] && nums[mid] >= nums[high]) {
-                low = mid + 1;
-            } else if (nums[mid] <= nums[low] && nums[mid] <= nums[high]) {
-                high = mid;
-            } else if (nums[mid] >= nums[low] && nums[mid] <= nums[high]) {
-                high = mid - 1;
+            System.out.println(low);
+            System.out.println(high);
+            int mid = Math.floorDiv(low + high, 2);
+            if (nums[mid] == target) {
+                return mid;
+            } else {
+                if (nums[mid] > nums[high]) {
+                    if (nums[mid] > target) {
+                        if (nums[low] > target) {
+                            low = mid + 1;
+                        } else {
+                            high = mid - 1;
+                        }
+
+                    } else {
+                        low = mid + 1;
+                    }
+                } else {
+                    if (nums[mid] > target) {
+                        high = mid - 1;
+                    } else {
+                        if (nums[high] >= target) {
+                            low = mid + 1;
+                        } else {
+                            high = mid - 1;
+                        }
+                    }
+                }
             }
         }
-        return low;
+        return nums[low] == target ? low : -1;
     }
-
 }
