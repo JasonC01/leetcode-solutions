ac2acc2bf867ac8896a943ce7d6fab6b03822425
CHUNG YUNSEONG <e0550449@u.nus.edu>
2024-11-20 11:05:05 +0800

    Time: 5 ms (85.71%), Space: 42 MB (67.8%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.java
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.java
@@ -1,38 +1,41 @@
 class Solution {
-    double ans = 0;
+    int[][] moves = new int[][]{new int[]{2, 1},
+            new int[]{2, -1},
+            new int[]{1, 2},
+            new int[]{-1, 2},
+            new int[]{-2, 1},
+            new int[]{-2, -1},
+            new int[]{1, -2},
+            new int[]{-1, -2}
+    };
     double[][][] memo;
     public double knightProbability(int n, int k, int row, int column) {
         memo = new double[n + 1][n + 1][k + 1];
-        knight(n, k, row, column, 1);
+        for (double[][] i : memo) {
+            for (double[] j : i) {
+                Arrays.fill(j, -1);
+            }
+        }
+        return dfs(n, k, row, column, 1.0);
+    }
 
+    public double dfs(int n, int k, int y, int x, double prob) {
+        if (x < 0 || y < 0 || x > n - 1 || y > n - 1) {
+            return 0;
+        }
+        if (k == 0) {
+            return prob;
+        }
+        if (memo[y][x][k] != -1) {
+            return memo[y][x][k];
+        }
+        double ans = 0;
+        for (int[] move : moves) {
+            ans += dfs(n, k - 1, y + move[0], x + move[1], prob / 8);
+        }
+        memo[y][x][k] = ans;
         return ans;
     }
 
-    public double knight(int n, int k, int row, int column, double prob) {
-        if (row > n - 1 || row < 0 || column > n - 1 || column < 0) {
-        } else {
-            if (k == 0) {
-                ans += prob;
-                return prob;
-            } else {
-                if (memo[row][column][k] != 0) {
-                    ans += memo[row][column][k];
-                    return memo[row][column][k];
-                } else{
-                    double newProb = prob / 8;
-                    double res = knight(n, k - 1, row + 2, column + 1, newProb) +
-                    knight(n, k - 1, row + 2, column - 1, newProb) +
-                    knight(n, k - 1, row - 2, column + 1, newProb) +
-                    knight(n, k - 1, row - 2, column - 1, newProb) +
-                    knight(n, k - 1, row + 1, column + 2, newProb) +
-                    knight(n, k - 1, row - 1, column + 2, newProb) +
-                    knight(n, k - 1, row + 1, column - 2, newProb) +
-                    knight(n, k - 1, row - 1, column - 2, newProb);
-                    memo[row][column][k] = res;
-                    return res;
-                }
-            }
-        }
-        return 0;
-    }
+
 }
