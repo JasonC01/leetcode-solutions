8a739ae82afa42029393c8aa7bfd3729ce7ec6eb
CHUNG YUNSEONG <e0550449@u.nus.edu>
2024-11-20 12:17:04 +0800

    Time: 5 ms (5.03%), Space: 41.8 MB (5.39%) - LeetHub

--- 1137-height-checker/1137-height-checker.java
+++ 1137-height-checker/1137-height-checker.java
@@ -1,12 +1,24 @@
 class Solution {
     public int heightChecker(int[] heights) {
-        int[] h = Arrays.copyOf(heights, heights.length);
-        Arrays.sort(h);
-        int ans = 0;
+        int[][] h = new int[heights.length][2];
         for (int i = 0; i < heights.length; i++) {
-            if (heights[i] != h[i]) ans++;
+            h[i] = new int[]{heights[i], i};
+        }
+        for (int i = h.length - 1; i >= 0; i--) {
+            int curr = i;
+            while (curr < h.length - 1 && h[curr + 1][0] < h[curr][0]) {
+                int[] temp = h[curr];
+                h[curr] = h[curr + 1];
+                h[curr + 1] = temp;
+                curr++;
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < h.length; i++) {
+            if (h[i][1] != i && h[i][0] != heights[i]) {
+                ans++;
+            }
         }
         return ans;
     }
-
 }
