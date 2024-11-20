a74bfed58013588dafc1599d046a54533028d3b8
CHUNG YUNSEONG <e0550449@u.nus.edu>
2024-11-20 18:35:11 +0800

    Time: 21 ms (24.71%), Space: 46.4 MB (9.88%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,26 +1,37 @@
 class Solution {
     public int countSubstrings(String s) {
-        int ans = 0;
+        boolean[][] dp = new boolean[s.length() + 1][s.length() + 1];
         for (int i = 0; i < s.length(); i++) {
-            ans++;
-            int low = i - 1;
-            int high = i + 1;
-            while (low >= 0 && high < s.length() && s.charAt(low) == s.charAt(high)) {
-                low--;
-                high++;
-                ans++;
-            }
-            if (i < s.length() - 1 && s.charAt(i) == s.charAt(i + 1)) {
-                low = i - 1;
-                high = i + 2;
-                ans++;
-                while (low >= 0 && high < s.length() && s.charAt(low) == s.charAt(high)) {
-                    low--;
-                    high++;
-                    ans++;
+            dp[i][i] = true;
+        }
+        for (int i = s.length() - 1; i >= 0; i--) {
+            for (int j = i + 1; j < s.length(); j++) {
+                if (i == 0) {
+                    dp[i][j] = isPalindrome(s.substring(i, j + 1));
+                } else {
+                    if (j - i < 2) {
+                        dp[i][j] = s.charAt(i) == s.charAt(j);
+                    } else {
+                        dp[i][j] = s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1];
+                    }
                 }
             }
         }
+        int ans = 0;
+        for (boolean[] i : dp) {
+            for (boolean j : i) {
+                if (j) ans++;
+            }
+        }
         return ans;
+    }
+
+    public boolean isPalindrome(String s) {
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
