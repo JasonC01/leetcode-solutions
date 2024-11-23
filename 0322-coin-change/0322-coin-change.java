fa06d559b91aef9a7a7d7fb62612e3e1e560fe50
CHUNG YUNSEONG <e0550449@u.nus.edu>
2024-11-23 20:43:01 +0800

    Time: 17 ms (45%), Space: 43.7 MB (97.53%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,27 +1,22 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[] memo = new int[amount + 1];
-        Arrays.fill(memo, Integer.MAX_VALUE);
-        memo[0] = 0;
-        for (int coin : coins) {
-            if (coin <= amount) {
-                memo[coin] = 1;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i : coins) {
+            if (i <= amount) {
+                dp[i] = 1;
             }
         }
         for (int i = 1; i <= amount; i++) {
-            int val = Integer.MAX_VALUE;
-            for (int coin : coins) {
-                if (i < coin) {
-                    continue;
-                }
-                int counterpart = memo[i - coin];
-                if (counterpart == Integer.MAX_VALUE) {
-                    continue;
-                }
-                val = Math.min(val, counterpart + 1);
+            if (dp[i] == 1) {
+                continue;
             }
-            memo[i] = val;
+            for (int c : coins) {
+                int prev = i - c < 0 ? Integer.MAX_VALUE : dp[i - c];
+                if (prev != Integer.MAX_VALUE) dp[i] = Math.min(dp[i], prev + 1);
+            }
         }
-        return memo[amount] == Integer.MAX_VALUE ? -1 : memo[amount];
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
     }
 }
