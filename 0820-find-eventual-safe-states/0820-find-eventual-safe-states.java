4efa113be4e4f99abb9547ba404628d2d51ff4b8
CHUNG YUNSEONG <e0550449@u.nus.edu>
2025-01-24 16:19:57 +0800

    Time: 19 ms (47.74%), Space: 55.6 MB (53.71%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
@@ -1,34 +1,33 @@
 class Solution {
-    boolean[] isSafe;
-    boolean[] checked;
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        isSafe = new boolean[graph.length];
-        checked = new boolean[graph.length];
-        List<Integer> safeNodes = new ArrayList<>();
+        boolean[] terminalNodes = new boolean[graph.length];
+        boolean[] visited = new boolean[graph.length];
         for (int i = 0; i < graph.length; i++) {
-            if (checkSafe(graph, i, new boolean[graph.length]))  {
-                safeNodes.add(i);
-            }
+            terminalNodes[i] = graph[i].length == 0;
         }
-        return safeNodes;
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < graph.length; i++) {
+            if (isTerminal(graph, i, terminalNodes, visited)) ans.add(i);
+        }
+        return ans;
     }
 
-    public boolean checkSafe(int[][] graph, int currNode, boolean[] visited) {
-        if (graph[currNode].length == 0) {
-            return isSafe[currNode] = true;
-        }
-        if (checked[currNode]) {
-            return isSafe[currNode];
+    public boolean isTerminal(int[][] graph, int currNode, boolean[] terminalNodes, boolean[] visited) {
+        if (terminalNodes[currNode]) {
+            return true;
         }
         if (visited[currNode]) {
             return false;
         }
-        boolean ans = true;
         visited[currNode] = true;
         for (int adj : graph[currNode]) {
-            ans &= checkSafe(graph, adj, visited);
+            if (!isTerminal(graph, adj, terminalNodes, visited)) {
+                visited[currNode] = false;
+                return false;
+            }
         }
-        checked[currNode] = true;
-        return isSafe[currNode] = ans;
+        visited[currNode] = false;
+        terminalNodes[currNode] = true;
+        return true;
     }
 }
