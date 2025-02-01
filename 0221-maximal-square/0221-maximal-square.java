7c5bea3e37f4b130039cc37646d195e3a3ff5bac
CHUNG YUNSEONG <e0550449@u.nus.edu>
2025-02-02 00:14:40 +0800

    Time: 7 ms (93.28%), Space: 60.5 MB (35.78%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -1,47 +1,25 @@
 class Solution {
     public int maximalSquare(char[][] matrix) {
-        int low = 0;
-        int high = Math.min(matrix.length, matrix[0].length);
-        while (low < high) {
-            int mid = (int) Math.ceil((low + high) / 2.0);
-            if (isPossible(matrix, mid)) {
-                low = mid;
-            } else {
-                high = mid - 1;
-            }
-        }
-        return low * low;
-    }
-    
-    public boolean isPossible(char[][] matrix, int squareSize) {
-        int m = matrix.length;
+        int m = matrix.length; 
         int n = matrix[0].length;
-        boolean[][] isRowPossible = new boolean[m][n - squareSize + 1];
-        for (int i = 0; i < m; i++) {
-            int currSum = 0;
-            for (int j = 0; j < squareSize; j++) {
-                currSum += matrix[i][j] == '1' ? 1 : 0;
-            }
-            isRowPossible[i][0] = currSum == squareSize;
-            for (int j = 1; j < n - squareSize + 1; j++) {
-                currSum -= matrix[i][j - 1] == '1' ? 1 : 0;
-                currSum += matrix[i][j + squareSize - 1] == '1' ? 1 : 0;
-                isRowPossible[i][j] = currSum == squareSize;
-            }
+        int[][] dp = new int[m][n];
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            dp[m - 1][i] = matrix[m - 1][i] == '1' ? 1 : 0;
+            ans = Math.max(ans, dp[m - 1][i]);
         }
-        for (int i = 0; i < n - squareSize + 1; i++) {
-            for (int j = 0; j < m - squareSize + 1; j++) {
-                if (isRowPossible[j][i]) {
-                    boolean possible = true;
-                    for (int k = j; k < j + squareSize; k++) {
-                        possible &= isRowPossible[k][i];
-                    }
-                    if (possible) {
-                        return true;
-                    }
+        for (int i = 0; i < m; i++) {
+            dp[i][n - 1] = matrix[i][n - 1] == '1' ? 1 : 0;
+            ans = Math.max(ans, dp[i][n - 1]);
+        }
+        for (int i = m - 2; i >= 0; i--) {
+            for (int j = n - 2; j >= 0; j--) {
+                if (matrix[i][j] == '1') {
+                    dp[i][j] = 1 + Math.min(Math.min(dp[i + 1][j + 1], dp[i + 1][j]), dp[i][j + 1]);
+                    ans = Math.max(ans, dp[i][j]);
                 }
             }
         }
-        return false;
+        return ans * ans;
     }
 }
