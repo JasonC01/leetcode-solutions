49c698253cad6819ecf84c871f55795d148ef912
CHUNG YUNSEONG <e0550449@u.nus.edu>
2025-02-11 19:29:39 +0800

    Time: 4 ms (19.96%), Space: 46 MB (91.44%) - LeetHub

--- 0879-maximize-distance-to-closest-person/0879-maximize-distance-to-closest-person.java
+++ 0879-maximize-distance-to-closest-person/0879-maximize-distance-to-closest-person.java
@@ -1,55 +1,20 @@
 class Solution {
     public int maxDistToClosest(int[] seats) {
-        List<Integer> arr = new ArrayList<>();
-        for (int i = 0; i < seats.length; i++) {
-            if (seats[i] == 1) {
-                arr.add(i);
-            }
+        int[] closestStart = new int[seats.length];
+        int[] closestEnd = new int[seats.length];
+        int n = seats.length;
+        closestStart[0] = seats[0] == 1 ? 0 : Integer.MAX_VALUE;
+        closestEnd[n - 1] = seats[n - 1] == 1 ? 0 : Integer.MAX_VALUE;
+        for (int i = 1; i < seats.length; i++) {
+            closestStart[i] = seats[i] == 1 ? 0 : closestStart[i - 1] == Integer.MAX_VALUE ? Integer.MAX_VALUE : closestStart[i - 1] + 1;
+            closestEnd[n - 1 - i] = seats[n - 1 - i] == 1 ? 0 : closestEnd[n - i] == Integer.MAX_VALUE ? Integer.MAX_VALUE : closestEnd[n - i] + 1;
         }
-        int ans = Integer.MIN_VALUE;
+        int ans = 0;
         for (int i = 0; i < seats.length; i++) {
             if (seats[i] == 0) {
-                int first = binarySearch(arr, i);
-                int second = binarySearch2(arr, i);
-                if (first == -1) {
-                    ans = Math.max(ans, second - i);
-                } else if (second == -1) {
-                    ans = Math.max(ans, i - first);
-                } else {
-                    ans = Math.max(ans, Math.min(i - binarySearch(arr, i), binarySearch2(arr, i) - i));
-                }
-
+                ans = Math.max(ans, Math.min(closestStart[i], closestEnd[i]));
             }
         }
         return ans;
-    }
-
-    public int binarySearch(List<Integer> seats, int currIndex) {
-        int low = 0;
-        int high = seats.size() - 1;
-
-        while (low < high) {
-            int mid = (int) Math.ceil((double) (low + high) / 2);
-            if (seats.get(mid) > currIndex) {
-                high = mid - 1;
-            } else {
-                low = mid;
-            }
-        }
-        return seats.get(low) >= currIndex ? -1 : seats.get(low);
-    }
-
-    public int binarySearch2(List<Integer> seats, int currIndex) {
-        int low = 0;
-        int high = seats.size() - 1;
-        while (low < high) {
-            int mid = (int) Math.floor((double) (low + high) / 2);
-            if (seats.get(mid) > currIndex) {
-                high = mid;
-            } else {
-                low = mid + 1;
-            }
-        }
-        return seats.get(low) <= currIndex ? -1 : seats.get(low);
     }
 }
