f6c6a1b12dd472d3e23156e6c03e8099bd2602f5
CHUNG YUNSEONG <e0550449@u.nus.edu>
2025-02-20 22:58:10 +0800

    Time: 0 ms (100%), Space: 41.7 MB (47.65%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.java
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.java
@@ -1,23 +1,13 @@
 class Solution {
     public String findDifferentBinaryString(String[] nums) {
-        HashSet<String> set = new HashSet<>();
-        Collections.addAll(set, nums);
-        return helper(new StringBuilder(), nums[0].length(), set);
-    }
-    
-    public String helper(StringBuilder sb, int n, HashSet<String> set) {
-        if (sb.length() == n) {
-            return !set.contains(sb.toString()) ? sb.toString() : "";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < nums[0].length(); i++) {
+            if (i <= nums.length - 1) {
+                sb.append(nums[i].charAt(i) == '1' ? '0' : '1');
+            } else {
+                sb.append('0');
+            }
         }
-        sb.append('0');
-        String zeroAdded = helper(sb, n, set);
-        sb.deleteCharAt(sb.length() - 1);
-        if (!zeroAdded.isEmpty()) {
-            return zeroAdded;
-        }
-        sb.append('1');
-        String oneAdded = helper(sb, n, set);
-        sb.deleteCharAt(sb.length() - 1);
-        return oneAdded;
+        return sb.toString();
     }
 }
