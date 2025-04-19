4511c7e446a33882fa4feb3ca6f54d71ff45339c
CHUNG YUNSEONG <e0550449@u.nus.edu>
2025-04-19 15:05:37 +0800

    Time: 168 ms (5.68%), Space: 57.7 MB (21.34%) - LeetHub

--- 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.java
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.java
@@ -1,43 +1,47 @@
 class Solution {
-    public long countFairPairs(int[] nums, int lower, int upper) {
+     public long countFairPairs(int[] nums, int lower, int upper) {
         Arrays.sort(nums);
         long ans = 0;
-        for (int i = 0; i < nums.length; i++) {
-            int low = bSearchLow(nums, lower - nums[i]);
-            int high = bSearchHigh(nums, upper - nums[i]);
-            if (low == -1 || high == -1) {
-                continue;
-            }
-            ans += (i >= low && i <= high) ? (high - low) : (high - low + 1);
+        for (int i = 0; i < nums.length - 1; i++) {
+            int curr = nums[i];
+            int low = bSearchLower(nums, i + 1, lower, curr);
+            int high = bSearchUpper(nums, i + 1, upper, curr);
+            if (curr + nums[low] < lower || curr + nums[high] > upper) continue;
+            ans += (high - low + 1);
         }
-        return ans / 2;
+        return ans;
     }
-
-    public int bSearchLow(int[] nums, int target) {
-        int low = 0;
+    
+    public int bSearchLower(int[] nums, int l, int lower, int curr) {
+        int low = l;
         int high = nums.length - 1;
         while (low < high) {
             int mid = Math.floorDiv(low + high, 2);
-            if (nums[mid] < target) {
+            if (nums[mid] + curr < lower) {
                 low = mid + 1;
             } else {
                 high = mid;
             }
         }
-        return nums[low] >= target ? low : -1;
+        return low;
     }
 
-    public int bSearchHigh(int[] nums, int target) {
-        int low = 0;
+    public int bSearchUpper(int[] nums, int l, int upper, int curr) {
+        int low = l;
         int high = nums.length - 1;
         while (low < high) {
             int mid = (int) Math.ceil((low + high) / 2.0);
-            if (nums[mid] > target) {
+            if (nums[mid] + curr > upper) {
                 high = mid - 1;
             } else {
                 low = mid;
             }
         }
-        return nums[low] <= target ? low : -1;
+        return low;
     }
 }
+
+
+// For each element i in nums:
+// Need to find other elements in range [lower - i, upper - i]
+// 
