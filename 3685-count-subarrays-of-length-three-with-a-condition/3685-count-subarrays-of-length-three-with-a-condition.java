f68a16901b21c5882f5a42e85b2f623989a619b6
CHUNG YUNSEONG <e0550449@u.nus.edu>
2025-04-27 20:36:01 +0800

    Time: 1 ms (100%), Space: 45.2 MB (40.73%) - LeetHub

---
+++ 3685-count-subarrays-of-length-three-with-a-condition/3685-count-subarrays-of-length-three-with-a-condition.java
@@ -0,0 +1,9 @@
+class Solution {
+     public int countSubarrays(int[] nums) {
+        int ans = 0;
+        for (int i = 2; i < nums.length; i++) {
+            if (nums[i - 1] % 2 == 0 && nums[i - 2] + nums[i] == (nums[i - 1] / 2)) ans++;
+        }
+        return ans;
+    }
+}
