0c13bc5dd57bc73bfef143395e92c3443bb44a36
CHUNG YUNSEONG <e0550449@u.nus.edu>
2025-05-07 14:49:06 +0800

    Time: 37 ms (5.61%), Space: 45.2 MB (41.74%) - LeetHub

---
+++ 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minTimeToReach(int[][] moveTime) {
+        int n = moveTime.length;
+        int m = moveTime[0].length;
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparing(x -> x[2]));
+        pq.add(new int[]{0, 0, 0});
+        boolean[][] visited = new boolean[n][m];
+        while (!pq.isEmpty()) {
+            int[] curr = pq.poll();
+            int x = curr[0];
+            int y = curr[1];
+            int time = curr[2];
+            if (y < 0 || x < 0 || y > n - 1 || x > m - 1 || visited[y][x]) continue;
+            visited[y][x] = true;
+            if (y == n - 1 && x == m - 1) {
+                return time;
+            }
+            for (int i = -1; i <= 1; i++) {
+                if (i != 0) {
+                    if (x + i < m && x + i >= 0) pq.add(new int[]{x + i, y, Math.max(time, moveTime[y][x + i]) + 1});
+                    if (y + i < n && y + i >= 0) pq.add(new int[]{x, y + i, Math.max(time, moveTime[y + i][x]) + 1});
+                }
+            }
+        }
+        return 0;
+    }
+}
