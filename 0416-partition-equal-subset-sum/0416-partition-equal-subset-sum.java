f9f655d74c432d51fd6cd1bf1cb781fe59631666
CHUNG YUNSEONG <e0550449@u.nus.edu>
2025-05-09 14:05:18 +0800

    Time: 69 ms (24.77%), Space: 72.4 MB (7.08%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -5,26 +5,17 @@ class Solution {
         for (int i : nums) {
             total += i;
         }
-        if (total % 2 == 1) {
-            return false;
-        }
-        memo = new int[nums.length][total + 1];
-        return helper(nums, 0, 0, total / 2);
+        memo = new int[nums.length + 1][total + 1];
+        return helper(nums, 0, 0, total);
     }
 
-    public boolean helper(int[] nums, int index, int currentSum, int target) {
-        if (currentSum == target) {
-            return true;
-        }
-        if (index > nums.length - 1 || currentSum > target) {
-            return false;
-        }
-        if (memo[index][currentSum] != 0) {
-            return memo[index][currentSum] == 1;
-        }
-        boolean res = helper(nums, index + 1, currentSum + nums[index], target);
-        res |= helper(nums, index + 1, currentSum, target);
-        memo[index][currentSum] = res ? 1 : -1;
+    public boolean helper(int[] nums, int index, int sum, int total) {
+        if (index > nums.length - 1) return sum * 2 == total;
+        if (sum * 2 == total) return true;
+        if (sum * 2 > total) return false;
+        if (memo[index][sum] != 0) return memo[index][sum] == 1;
+        boolean res = helper(nums, index + 1, sum + nums[index], total) || helper(nums, index + 1, sum, total);
+        memo[index][sum] = res ? 1 : -1;
         return res;
     }
 }
