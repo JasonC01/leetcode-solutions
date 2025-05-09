7d3b77103ff5d342e7b5807e9c543213862091ad
CHUNG YUNSEONG <e0550449@u.nus.edu>
2025-05-09 14:18:51 +0800

    Time: 0 ms (100%), Space: 41.3 MB (34.94%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.java
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -3,23 +3,19 @@ class Solution {
     public int combinationSum4(int[] nums, int target) {
         memo = new int[target + 1];
         Arrays.fill(memo, -1);
-        return dfs(nums, target);
+        return helper(nums, target, 0);
     }
-    
-    public int dfs(int[] nums, int target) {
-        if (target == 0) {
-            return 1;
-        }
-        if (target < 0) {
-            return 0;
-        }
-        if (memo[target] != -1) {
-            return memo[target];
-        }
+
+    public int helper(int[] nums,  int target, int sum) {
+        if (sum == target) return 1;
+        if (sum > target) return 0;
+        if (memo[sum] != -1) return memo[sum];
         int ans = 0;
         for (int i : nums) {
-            ans += dfs(nums, target - i);
+            ans += helper(nums, target, sum + i);
         }
-        return memo[target] = ans;
+        memo[sum] = ans;
+        return ans;
     }
+
 }
