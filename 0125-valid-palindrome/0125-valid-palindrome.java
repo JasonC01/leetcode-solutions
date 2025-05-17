b9bfa70eb7b83896b3fd0b655b9d95ba5c1ade1e
CHUNG YUNSEONG <e0550449@u.nus.edu>
2025-05-17 21:06:28 +0800

    Time: 15 ms (21.88%), Space: 44.8 MB (43.76%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,33 +1,10 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int low = 0;
-        int high = s.length() - 1;
+        s = s.replaceAll("[^A-Za-z0-9]", "");
         s = s.toLowerCase();
-        while (low < high) {
-            if (isAlphaNumeric(s.charAt(low)) && isAlphaNumeric(s.charAt(high))) {
-                if (s.charAt(low) == s.charAt(high)) {
-                    low++;
-                    high--;
-                } else {
-                    return false;
-                }
-            } else if (!isAlphaNumeric(s.charAt(low))) {
-                low++;
-            } else if (!isAlphaNumeric(s.charAt(high))) {
-                high--;
-            }
-        }
-        return true;
-    }
-
-    public boolean isAlphaNumeric(Character c) {
-        if (c >= 97 && c <= 122) {
-            return true;
-        }
-        try {
-            Integer.parseInt(String.valueOf(c));
-        } catch (Exception e) {
-            return false;
+        int length = s.length();
+        for (int i = 0; i < length / 2; i++) {
+            if (s.charAt(i) != s.charAt(length - 1 - i)) return false;
         }
         return true;
     }
