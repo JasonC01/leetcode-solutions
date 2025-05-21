53d75118bf4052f5ab739a27e3333f65ffd8d4e6
CHUNG YUNSEONG <e0550449@u.nus.edu>
2025-05-21 19:28:06 +0800

    Time: 13 ms (5.67%), Space: 45.6 MB (43.53%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.java
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.java
@@ -1,38 +1,28 @@
 class Solution {
     public List<String> findItinerary(List<List<String>> tickets) {
         HashMap<String, List<String>> adjList = new HashMap<>();
-        HashMap<String, Integer> inDegree = new HashMap<>();
-        HashMap<String, Integer> outDegree = new HashMap<>();
         for (List<String> ticket : tickets) {
-            if (!adjList.containsKey(ticket.get(0))) {
-                adjList.put(ticket.get(0), new ArrayList<>());
-            }
-            inDegree.merge(ticket.get(1),1, (a, b) -> a + 1);
-            outDegree.merge(ticket.get(0), 1, (a, b) -> a + 1);
-            adjList.get(ticket.get(0)).add(ticket.get(1));
+            String from = ticket.get(0);
+            String to = ticket.get(1);
+            adjList.computeIfAbsent(from, x -> new ArrayList<>());
+            adjList.get(from).add(to);
         }
-        Stack<String> q = new Stack<>();
-        q.add("JFK");
-        for (List<String> s : adjList.values()) {
-            s.sort(Comparator.comparing(x -> x));
+        for (List<String> values : adjList.values()) {
+            values.sort(Comparator.comparing(x -> x));
         }
-        Stack<String> res = new Stack<>();
-        
-        // System.out.println(adjList);
-        while (!q.isEmpty()) {
-            // System.out.println(q);
-            String curr = q.peek();
+        Stack<String> stack = new Stack<>();
+        stack.push("JFK");
+        Stack<String> ans = new Stack<>();
+        while (!stack.isEmpty()) {
+            String curr = stack.peek();
             if (adjList.getOrDefault(curr, new ArrayList<>()).isEmpty()) {
-                // System.out.println(curr);
-                res.add(q.pop());
+                ans.add(stack.pop());
             } else {
-                q.add(adjList.get(curr).remove(0));
+                stack.push(adjList.get(curr).remove(0));
             }
         }
-        List<String> ans = new ArrayList<>();
-        while (!res.isEmpty()) {
-            ans.add(res.pop());
-        }
-        return ans;
+        List<String> realAns = new ArrayList<>();
+        while (!ans.isEmpty()) realAns.add(ans.pop());
+        return realAns;
     }
 }
