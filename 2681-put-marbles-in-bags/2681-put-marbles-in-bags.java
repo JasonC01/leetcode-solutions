cbf6438734f5b4e2d81e03f38e18a80c982362a3
CHUNG YUNSEONG <e0550449@u.nus.edu>
2025-05-21 17:39:45 +0800

    Time: 151 ms (6.55%), Space: 55.7 MB (81.83%) - LeetHub

--- 2681-put-marbles-in-bags/2681-put-marbles-in-bags.java
+++ 2681-put-marbles-in-bags/2681-put-marbles-in-bags.java
@@ -2,15 +2,13 @@ class Solution {
     public long putMarbles(int[] weights, int k) {
         List<Integer> arr = new ArrayList<>();
         for (int i = 0; i < weights.length - 1; i++) {
-            arr.add(weights[i] + weights[i + 1]);
+            arr.add(weights[i + 1] + weights[i]);
         }
         arr.sort(Comparator.comparing(x -> x));
-        long max = 0;
-        long min = 0;
+        long ans = 0;
         for (int i = 0; i < k - 1; i++) {
-            max += arr.get(arr.size() - 1 - i);
-            min += arr.get(i);
+            ans += arr.get(arr.size() - 1 - i) - arr.get(i);   
         }
-        return max -min;
+        return ans;
     }
 }
