46b81ef92f83f88081398b6cebe3f73b3aa2cf0a
CHUNG YUNSEONG <e0550449@u.nus.edu>
2025-05-28 19:54:03 +0800

    Time: 103 ms (5.15%), Space: 120 MB (88.09%) - LeetHub

--- 3348-minimum-cost-walk-in-weighted-graph/3348-minimum-cost-walk-in-weighted-graph.java
+++ 3348-minimum-cost-walk-in-weighted-graph/3348-minimum-cost-walk-in-weighted-graph.java
@@ -1,57 +1,57 @@
 class Solution {
     public int[] minimumCost(int n, int[][] edges, int[][] query) {
         DSU dsu = new DSU();
-        int[] ans = new int[query.length];
+        HashMap<Integer, HashMap<Integer, Integer>> memo = new HashMap<>();
+        int[] parents = new int[n];
         for (int[] edge : edges) {
-            int from = edge[0];
-            int to = edge[1];
-            int weight = edge[2];
-            dsu.join(from, to, weight);
+            //     System.out.println(dsu.parentMap);
+            // System.out.println(dsu.valueMap);
+            dsu.join(edge[0], edge[1], edge[2]);
         }
-        System.out.println(dsu.parentMap);
-        System.out.println(dsu.minWeightMap);
+        // System.out.println(dsu.parentMap);
+        // System.out.println(dsu.valueMap);
+        int[] ans = new int[query.length];
         for (int i = 0; i < query.length; i++) {
-            int firstParent = dsu.find(query[i][0]);
-            int secondParent = dsu.find(query[i][1]);
-            ans[i] = firstParent != -1 && firstParent == secondParent ? dsu.minWeightMap.getOrDefault(firstParent, -1) : -1;
+            int[] currQuery = query[i];
+            int from = dsu.find(currQuery[0]);
+            int to = dsu.find(currQuery[1]);
+            if (from == to && from != -1) {
+                ans[i] = dsu.valueMap.get(from);
+            } else {
+                ans[i] = -1;
+            }
         }
         return ans;
     }
 
     public class DSU {
         HashMap<Integer, Integer> parentMap = new HashMap<>();
-        HashMap<Integer, Integer> minWeightMap = new HashMap<>();
+        HashMap<Integer, Integer> valueMap = new HashMap<>();
 
-        public DSU() {}
-
-        public void join(int x, int y, int weight) {
-            int parentX = find(x);
-            int parentY = find(y);
-            if (parentY == -1 && parentX == -1) {
-                parentMap.put(x, x);
-                parentMap.put(y, x);
-                minWeightMap.merge(x, weight, (a, b) -> a & weight);
-            } else if (parentX == -1) {
-                parentMap.put(x, parentY);
-                minWeightMap.merge(parentY, weight, (a, b) -> a & weight);
-            } else if (parentY == -1) {
-                parentMap.put(y, parentX);
-                minWeightMap.merge(parentX, weight, (a, b) -> a & weight);
-            } else {
-                parentMap.put(parentY, parentX);
-                minWeightMap.merge(parentX, weight, (a, b) -> a & weight & minWeightMap.get(parentY));
-            }
+        public DSU(){
+        }
+        public int find(int i) {
+            if (!parentMap.containsKey(i)) return -1;
+            return parentMap.get(i) == i ? i : find(parentMap.get(i));
         }
 
-        public int find(int i) {
-            if (!parentMap.containsKey(i)) {
-                return -1;
+        public void join(int x, int y, int value) {
+            int parentX = find(x);
+            int parentY = find(y);
+            if (parentX == -1 && parentY == -1) {
+                parentMap.put(y, x);
+                parentMap.put(x, x);
+                valueMap.put(x, value);
+            } else if (parentX == -1) {
+                parentMap.put(x, parentY);
+                valueMap.merge(parentY, value, (a, b) -> a & b);
+            } else if (parentY == -1) {
+                parentMap.put(y, parentX);
+                valueMap.merge(parentX, value, (a, b) -> a & b);
+            } else {
+                parentMap.put(parentY, parentX);
+                valueMap.merge(parentX, value & valueMap.getOrDefault(parentY, value), (a, b) -> a & b);
             }
-            int parent = parentMap.get(i);
-            while (parent != parentMap.get(parent)) {
-                parent = parentMap.get(parent);
-            }
-            return parent;
         }
     }
 }
