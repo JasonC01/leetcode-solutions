6f6b2f01279520b55f4374f5979b03047ee162d1
CHUNG YUNSEONG <e0550449@u.nus.edu>
2025-05-28 15:33:18 +0800

    Time: 1 ms (48.62%), Space: 44.7 MB (92.59%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,17 +1,16 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        boolean[][] visited = new boolean[image.length][image[0].length];
+        int n = image.length;
+        int m = image[0].length;
+        int initialColor = image[sr][sc];
+        if (initialColor == color) return image;
         Queue<int[]> q = new ArrayDeque<>();
         q.add(new int[]{sr, sc});
-        int initColor = image[sr][sc];
         while (!q.isEmpty()) {
             int[] curr = q.poll();
             int y = curr[0];
             int x = curr[1];
-            if (y < 0 || y > image.length - 1 || x < 0 || x > image[0].length - 1 || visited[y][x] || image[y][x] != initColor) {
-                continue;
-            }
-            visited[y][x] = true;
+            if (y < 0 || x < 0 || y > n - 1 || x > m - 1 || image[y][x] != initialColor) continue;
             image[y][x] = color;
             q.add(new int[]{y + 1, x});
             q.add(new int[]{y - 1, x});
