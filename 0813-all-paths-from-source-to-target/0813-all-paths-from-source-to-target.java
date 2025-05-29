5375dad5a8c4aad93e697f2026b545e4e04ea6d4
CHUNG YUNSEONG <e0550449@u.nus.edu>
2025-05-29 20:40:25 +0800

    Time: 3 ms (27.51%), Space: 46.8 MB (20.09%) - LeetHub

--- 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.java
+++ 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.java
@@ -1,25 +1,22 @@
 class Solution {
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        return dfs(graph, 0, new HashMap<>());
+        return dfs(graph, 0, new LinkedList<>());
     }
 
-    public List<List<Integer>> dfs(int[][] graph, int currNode, HashMap<Integer, List<List<Integer>>> memo) {
+    public List<List<Integer>> dfs(int[][] graph, int currNode, LinkedList<Integer> path) {
         List<List<Integer>> ans = new ArrayList<>();
         if (currNode == graph.length - 1) {
-            List<Integer> res = new ArrayList<>();
-            res.add(currNode);
-            ans.add(res);
+            path.addLast(currNode);
+            ans.add(new ArrayList<>(path));
+            path.removeLast();
             return ans;
         }
-        // if (memo.containsKey(currNode)) return memo.get(currNode);
+        path.addLast(currNode);
         for (int adj : graph[currNode]) {
-            List<List<Integer>> res = dfs(graph, adj, memo);
-            for (List<Integer> path : res) {
-                List<Integer> newPath = new ArrayList<>(path);
-                newPath.add(0, currNode);
-                ans.add(newPath);
-            }
+            ans.addAll(dfs(graph, adj, path));
         }
+        path.removeLast();
         return ans;
     }
+
 }
