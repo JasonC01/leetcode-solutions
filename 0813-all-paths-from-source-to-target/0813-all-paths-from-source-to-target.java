2bc45fd10be75e817e09e23e397908a17d13eca6
CHUNG YUNSEONG <e0550449@u.nus.edu>
2025-05-29 20:36:36 +0800

    Time: 9 ms (11.21%), Space: 46.6 MB (36.91%) - LeetHub

---
+++ 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        return dfs(graph, 0, new HashMap<>());
+    }
+
+    public List<List<Integer>> dfs(int[][] graph, int currNode, HashMap<Integer, List<List<Integer>>> memo) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (currNode == graph.length - 1) {
+            List<Integer> res = new ArrayList<>();
+            res.add(currNode);
+            ans.add(res);
+            return ans;
+        }
+        if (memo.containsKey(currNode)) return memo.get(currNode);
+        for (int adj : graph[currNode]) {
+            List<List<Integer>> res = dfs(graph, adj, memo);
+            for (List<Integer> path : res) {
+                List<Integer> newPath = new ArrayList<>(path);
+                newPath.add(0, currNode);
+                ans.add(newPath);
+            }
+        }
+        return ans;
+    }
+}
