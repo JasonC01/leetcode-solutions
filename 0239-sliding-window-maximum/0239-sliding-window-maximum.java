1d5a0c3ebbe7d5fce7c4ddaed551331b0c7ecb28
CHUNG YUNSEONG <e0550449@u.nus.edu>
2025-06-06 16:11:58 +0800

    Time: 320 ms (5.01%), Space: 59.5 MB (46.48%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,19 +1,16 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(b[0], a[0]));
-        for (int i = 0; i < k; i++) {
-            pq.add(new int[]{nums[i], i});
-        }
+        TreeMap<Integer, Integer> counter = new TreeMap<>((a, b) -> Integer.compare(b, a));
         int[] ans = new int[nums.length - k + 1];
-        int nextIndex = k;
-        ans[0] = pq.peek()[0];
-        for (int i = 1; i < ans.length; i++) {
-            while (!pq.isEmpty() && pq.peek()[1] <= nextIndex - k) {
-                pq.poll();
-            }
-            pq.add(new int[]{nums[nextIndex], nextIndex});
-            nextIndex++;
-            ans[i] = pq.peek()[0];
+        for (int i = 0; i < k; i++) {
+            counter.merge(nums[i], 1, (a, b) -> a + 1);
+        }
+        ans[0] = counter.firstKey();
+        for (int i = k; i < nums.length; i++) {
+            counter.merge(nums[i - k], 0, (a, b) -> a - 1);
+            if (counter.get(nums[i - k]) == 0) counter.remove(nums[i - k]);
+            counter.merge(nums[i], 1, (a, b) -> a + 1);
+            ans[i - k + 1] = counter.firstKey();
         }
         return ans;
     }
