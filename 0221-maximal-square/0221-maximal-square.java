6b491f0dcfecd207442e9dd84be9967356cbe1ea
CHUNG YUNSEONG <e0550449@u.nus.edu>
2025-06-07 15:23:37 +0800

    Time: 33 ms (7.98%), Space: 59.1 MB (84.61%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -1,25 +1,24 @@
 class Solution {
     public int maximalSquare(char[][] matrix) {
-        int m = matrix.length; 
-        int n = matrix[0].length;
-        int[][] dp = new int[m][n];
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            dp[m - 1][i] = matrix[m - 1][i] == '1' ? 1 : 0;
-            ans = Math.max(ans, dp[m - 1][i]);
-        }
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int max = 0;
+        int[][] dp = new int[n][m];
         for (int i = 0; i < m; i++) {
-            dp[i][n - 1] = matrix[i][n - 1] == '1' ? 1 : 0;
-            ans = Math.max(ans, dp[i][n - 1]);
+            dp[n - 1][i] = matrix[n - 1][i] == '1' ? 1 : 0;
+            max = Math.max(max, dp[n - 1][i]);
         }
-        for (int i = m - 2; i >= 0; i--) {
-            for (int j = n - 2; j >= 0; j--) {
-                if (matrix[i][j] == '1') {
-                    dp[i][j] = 1 + Math.min(Math.min(dp[i + 1][j + 1], dp[i + 1][j]), dp[i][j + 1]);
-                    ans = Math.max(ans, dp[i][j]);
-                }
+        for (int i = 0; i < n; i++) {
+            dp[i][m - 1] = matrix[i][m - 1] == '1' ? 1 : 0;
+            max = Math.max(max, dp[i][m - 1]);
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = m - 2; j >= 0; j--) {
+                if (matrix[i][j] == '1') dp[i][j] = Math.min(dp[i + 1][j], Math.min(dp[i][j + 1], dp[i + 1][j + 1])) + 1;
+                max = Math.max(max, dp[i][j]);
             }
         }
-        return ans * ans;
+        System.out.println(Arrays.deepToString(dp));
+        return max * max;
     }
 }
