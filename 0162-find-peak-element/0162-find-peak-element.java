364f83b60c6e9aad5b59cf6686ced7461b521ada
CHUNG YUNSEONG <e0550449@u.nus.edu>
2025-06-07 14:10:52 +0800

    Time: 0 ms (100%), Space: 42.9 MB (7.41%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,27 +1,17 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        if (nums.length == 1) {
-            return 0;
+        if (nums.length == 1) return 0;
+        if (nums.length == 2) return nums[0] > nums[1] ? 0 : 1;
+        if (nums[0] > nums[1]) return 0;
+        if (nums[nums.length - 1] > nums[nums.length - 2]) return nums.length - 1;
+        int low = 1;
+        int high = nums.length - 2;
+        while (low < high) {
+            int mid = Math.floorDiv(low + high, 2);
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) return mid;
+            else if (nums[mid] < nums[mid - 1]) high = mid - 1;
+            else low = mid + 1;
         }
-        if (nums[0] > nums[1]) {
-            return 0;
-        }
-        if (nums[nums.length - 1] > nums[nums.length - 2]) {
-            return nums.length - 1;
-        }
-        return helper(nums, 0, nums.length - 1);
-    }
-
-    public int helper(int[] nums, int low, int high) {
-        if (high <= low) {
-            return -1;
-        }
-        int mid = (low + high) / 2;
-        if (nums[mid] > (mid - 1 < 0 ? Integer.MIN_VALUE : nums[mid - 1]) && nums[mid] > (mid + 1 > nums.length - 1 ? Integer.MIN_VALUE : nums[mid + 1])) {
-            return mid;
-        }
-        int ans = helper(nums, low, mid);
-        ans = Math.max(ans, helper(nums, mid + 1, high));
-        return ans;
+        return low;
     }
 }
